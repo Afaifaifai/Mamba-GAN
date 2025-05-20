3a2d9ba641c02616fa6495694c71eaf0164ef98a
Afaifaifai <131575961+Afaifaifai@users.noreply.github.com>
2025-05-21 01:48:46 +0800

    Update Bert.py

--- Bert.py
+++ Bert.py
@@ -41,6 +41,25 @@ def create_bert_model(self, model_name_or_path, loss_type, model_type=None, rand
     return model.bert if loss_type == "mmd" else model
 
 
+def calculate_unfreeze_idx(self, cfg):
+    cn, unfreeze_idx, layers = 0, [], []
+    for name, param in self.discriminator.named_parameters():
+        if name.startswith("bert.embeddings") and not cfg.DISCRIMINATOR.BERT.random_weights:
+            pass
+        elif name.startswith("bert.encoder.layer") and name.split('.')[3] in cfg.DISCRIMINATOR.BERT.freeze_layers:
+            pass
+        else:
+            unfreeze_idx.append(cn)
+        cn += 1
+
+        if name.startswith("bert.encoder.layer"):
+            layers.append(name.split('.')[3])
+
+        # check the total number of layers in the BERT >= the number of layers to be freeze
+    assert len(layers) >= len(cfg.DISCRIMINATOR.BERT.freeze_layers)
+
+    return unfreeze_idx
+
 def main(cfg, vocab):
     ntokens = len(vocab)
         # Create discriminator
@@ -50,12 +69,12 @@ def main(cfg, vocab):
             cfg.DISCRIMINATOR.BERT.model_path, cfg.DISCRIMINATOR.BERT.loss_type, cfg.DISCRIMINATOR.BERT.model_type,
             cfg.DISCRIMINATOR.BERT.random_weights
         )
-        discriminator.unfreeze_idx = self.calculate_unfreeze_idx(cfg)
+        discriminator.unfreeze_idx = calculate_unfreeze_idx(cfg)
 
     temperature = 1
-    vocab = vocab
     vec_len = vocab.vec_len
 
 
 
 main()
+
