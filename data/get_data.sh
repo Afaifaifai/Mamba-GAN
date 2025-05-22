57929955f74909cef9a372e6a98e46302d60b451
Afaifaifai <131575961+Afaifaifai@users.noreply.github.com>
2025-05-22 17:57:54 +0800

    Update get_data.sh

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,2 +1,11 @@
 wget https://storage.googleapis.com/magentadata/datasets/maestro/v1.0.0/maestro-v1.0.0-midi.zip
 unzip maestro-v1.0.0-midi.zip
+
+ource /usr/local/miniconda3/etc/profile.d/conda.sh
+conda activate py37
+
+python3 music_encoder.py --encode_official_maestro \  
+    --mode midi_to_npy \  
+    --pitch_transpose_lower -3 \  
+    --pitch_transpose_upper 3 \  
+    --output_folder ./maestro_magenta_s5_t3
