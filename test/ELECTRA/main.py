155536f0ee21b498ea3ab867f608716bc3d1589e
Afaifaifai <131575961+Afaifaifai@users.noreply.github.com>
2025-06-18 00:19:08 +0800

    Update main.py

--- test/ELECTRA/main.py
+++ test/ELECTRA/main.py
@@ -51,6 +51,8 @@ from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
 
+from tokenization_midi import MIDITokenizer
+
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.53.0.dev0")
